d9f226d7963a7eab0a00648faae9acb6301e946d
GitHub Action <action@github.com>
2025-02-17 22:03:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/pawrequest/redscript4ij/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/pawrequest/redscript4ij/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pawrequest.redscript4ij.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
